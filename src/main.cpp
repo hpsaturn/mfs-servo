d82fb575edf9532694d725b41f60207a9ebd588b
Antonio Vanegas <hpsaturn@gmail.com>
2019-02-01 12:48:18 +0100

    conflict on MFS library with Servo on Display methods maybe

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,9 @@
 #include <Servo.h>
 
 Servo mservo;
+int pos = 0;
 
-void display(float value){
+void display(int value){
   MFS.write(value);
 }
 
@@ -39,8 +40,17 @@ void setup()
 
 void loop()
 {
-  delay(100);
-
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    display(pos);
+    mservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    display(pos);
+    mservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
 }
 //
 // END OF FILE
