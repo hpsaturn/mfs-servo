d64fdee4d8725805392d9c5280565b4b73bb5240
Antonio Vanegas <hpsaturn@gmail.com>
2020-06-21 21:24:47 +0200

    display working and buttons with multi servo alternative

--- src/main.cpp
+++ src/main.cpp
@@ -14,28 +14,37 @@
 Servo mservo6;
 Servo mservo9;
 
-int pos = 0;
+#define MAX_ANGLE 175
+#define MIN_ANGLE 20
 
-void checkStopConditions(byte btn) {
+int pos = MIN_ANGLE;
+bool stop = false;
+int servo_delay = 45;
+
+void display(int value) {
+    MFS.write(value);
+}
+
+void checkButtons(byte btn) {
 
     MFS.manualDisplayRefresh();
     MFS.isrCallBack();
 
     if (btn == BUTTON_1_PRESSED || btn == BUTTON_1_LONG_PRESSED) {
         Serial.println("btn1");
+        MFS.beep(3, 4, 3, 3); 
+        stop = !stop;
+        delay(100);
     } else if (btn == BUTTON_2_PRESSED || btn == BUTTON_2_LONG_PRESSED) {
         Serial.println("btn2");
+        if (servo_delay < 255) servo_delay++;
     } else if (btn == BUTTON_3_PRESSED || btn == BUTTON_3_LONG_PRESSED) {
         Serial.println("btn3");
+        if (servo_delay > 0) servo_delay--;
     }
+    
+    display(servo_delay);
 
-    int16_t x = (int16_t)(analogRead(POT_PIN) / 10);
-    MFS.write(analogRead(x));
-    //MFS.beep(1, x, 1);  // beep 1 times, 800 milliseconds on / 1000 off
-}
-
-void display(int value) {
-    MFS.write(value);
 }
 
 void setup() {
@@ -60,19 +69,19 @@ bool toggle;
 
 void servoLoop() {
     static uint_fast32_t timeStamp = 0;
-    if (millis() - timeStamp > 15) {
+    if (millis() - timeStamp > servo_delay) {
         timeStamp = millis();
         // in steps of 1 degree
         mservo6.write(pos);  // tell servo to go to position in variable 'pos'
         mservo9.write(pos);  // tell servo to go to position in variable 'pos'
-        if (pos > 180 || pos == 0) toggle = !toggle;
+        if (pos == MAX_ANGLE || pos == MIN_ANGLE) toggle = !toggle;
         if (toggle) pos++;
         else pos--;
     }
 }
 
 void loop() {
-    servoLoop();
+    if (!stop) servoLoop();
     byte btn = MFS.getButton();
-    checkStopConditions(btn);
+    checkButtons(btn);
 }
