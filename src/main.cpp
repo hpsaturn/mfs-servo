e1196998cba93797c8f7786df0e45d651b936519
Antonio Vanegas <hpsaturn@gmail.com>
2020-06-21 18:58:59 +0200

    MFS implementation without timer, servo lib incompatibility

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,38 @@
 // 0.1.2 Multi functional Shield support
 //
 // Released to the public domain
-
-#include <TimerOne.h>
-#include <Wire.h>
 #include <MultiFuncShield.h>
 #include <Servo.h>
+#include <Wire.h>
 
-Servo mservo;
+Servo mservo6;
+Servo mservo9;
+
 int pos = 0;
 
-void display(int value){
-  MFS.write(value);
+void checkStopConditions(byte btn) {
+
+    MFS.manualDisplayRefresh();
+    MFS.isrCallBack();
+
+    if (btn == BUTTON_1_PRESSED || btn == BUTTON_1_LONG_PRESSED) {
+        Serial.println("btn1");
+    } else if (btn == BUTTON_2_PRESSED || btn == BUTTON_2_LONG_PRESSED) {
+        Serial.println("btn2");
+    } else if (btn == BUTTON_3_PRESSED || btn == BUTTON_3_LONG_PRESSED) {
+        Serial.println("btn3");
+    }
+
+    int16_t x = (int16_t)(analogRead(POT_PIN) / 10);
+    MFS.write(analogRead(x));
+    //MFS.beep(1, x, 1);  // beep 1 times, 800 milliseconds on / 1000 off
 }
 
-void setup()
-{
+void display(int value) {
+    MFS.write(value);
+}
+
+void setup() {
     Serial.begin(115200);
     delay(10);
     Serial.println();
@@ -29,29 +46,33 @@ void setup()
     Serial.println("====== SERVO MFS TEST  ======");
     Serial.println("==============================");
 
-    Timer1.initialize();
-    MFS.initialize(&Timer1);    // initialize multifunction shield library
+    mservo6.attach(6);
+    mservo9.attach(9);
+
+    // Timer1.initialize();
+    // MFS.initialize(&Timer1);  // initialize multifunction shield library
+    MFS.initialize();  // initialize multifunction shield library
     MFS.write(0);
     Serial.println("== Multi Function Shield ready");
-
-    mservo.attach(9);
-
 }
 
-void loop()
-{
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    display(pos);
-    mservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    display(pos);
-    mservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+bool toggle;
+
+void servoLoop() {
+    static uint_fast32_t timeStamp = 0;
+    if (millis() - timeStamp > 15) {
+        timeStamp = millis();
+        // in steps of 1 degree
+        mservo6.write(pos);  // tell servo to go to position in variable 'pos'
+        mservo9.write(pos);  // tell servo to go to position in variable 'pos'
+        if (pos > 180 || pos == 0) toggle = !toggle;
+        if (toggle) pos++;
+        else pos--;
+    }
 }
-//
-// END OF FILE
-//
+
+void loop() {
+    servoLoop();
+    byte btn = MFS.getButton();
+    checkStopConditions(btn);
+}
